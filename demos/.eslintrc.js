b6c0d61f47232f1f0965371ade6ce539729501d9
John Pablo <john.davis.2015.03@gmail.com>
2023-07-21 23:40:15 -0500

    add eslint

---
+++ demos/.eslintrc.js
@@ -0,0 +1,26 @@
+/* eslint quotes: ["error", "double"] */
+
+module.exports = {
+  env: {
+    commonjs: true,
+    es2021: true,
+    node: true,
+    jest: true,
+  },
+  extends: "airbnb-base",
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: [".eslintrc.{js,cjs}"],
+      parserOptions: {
+        sourceType: "script",
+      },
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: "latest",
+  },
+  rules: {},
+};
